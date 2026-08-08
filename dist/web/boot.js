71f535f088d4438e29f3e0132a7fbab172f08aed
soulwax <soulwax-95@protonmail.com>
2026-08-09 00:32:16 +0200

    Pin acoustic planner release artifacts

--- dist/web/boot.js
+++ dist/web/boot.js
@@ -3,7 +3,7 @@
   if ((WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,95,1,120,0])))) {
     try {
       const m = await import(app + 'main.07cbcdbf.mjs');
-      const compiled = await m.compileStreaming(fetch(app + 'main.29fb3534.wasm'));
+      const compiled = await m.compileStreaming(fetch(app + 'main.0f035823.wasm'));
       (await compiled.instantiate({})).invokeMain();
       return;
     } catch (e) {
@@ -11,6 +11,6 @@
     }
   }
   const s = document.createElement('script');
-  s.src = app + 'main.bf375f33.js';
+  s.src = app + 'main.abfadeb4.js';
   document.body.appendChild(s);
 })();
